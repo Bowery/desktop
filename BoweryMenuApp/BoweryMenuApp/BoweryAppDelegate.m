85263930a7a4bc242d7514966e29582e70cbbaa9
Zachary Hamed <zachhamed@gmail.com>
2014-07-10 16:41:09 -0400

    Button added and runs function on click

--- BoweryMenuApp/BoweryMenuApp/BoweryAppDelegate.m
+++ BoweryMenuApp/BoweryMenuApp/BoweryAppDelegate.m
@@ -11,10 +11,22 @@
 
 @implementation BoweryAppDelegate
 
+@synthesize refreshBtn = _refreshBtn;
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:32055/"]];
 	[self.webView.mainFrame loadRequest:request];
+}
+
+- (IBAction)doSomething:(id)sender {
+    if ([[_refreshBtn title] isEqualTo:@"Button"]) {
+        [_refreshBtn setTitle:@"Hi Steve"];
+    }
+    else {
+        [_refreshBtn setTitle:@"Button"];
+    }
+
 }
 
 @end
