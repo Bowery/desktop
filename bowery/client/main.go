5effac43212b5625d3e07aa24b45cf1a5d376ef4
Steve Kaliski <sjkaliski@gmail.com>
2014-07-21 13:24:22 -0400

    save

--- bowery/client/main.go
+++ bowery/client/main.go
@@ -1,12 +1,14 @@
 // Copyright 2013-2014 Bowery, Inc.
-// Contains the main entry point
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"path/filepath"
 
+	"github.com/Bowery/gopackages/localdb"
 	"github.com/Bowery/gopackages/schemas"
+	"github.com/Bowery/gopackages/sys"
 	"github.com/codegangsta/negroni"
 	"github.com/unrolled/render"
 )
@@ -14,77 +16,46 @@ import (
 var (
 	AuthEndpoint   string = "broome.io"
 	DaemonEndpoint string = "localhost:3000" // TODO (thebyrd) change this to match the toolbar app
-	Me             schemas.Developer
+	db             *localdb.DB
+	data           *localData
+	r              = render.New(render.Options{
+		IndentJSON:    true,
+		IsDevelopment: true,
+	})
 )
 
-type Application struct {
-	ID         string // normalized as 80-maspeth-ave, but user enters 80 Maspeth Ave
-	Name       string
-	Start      string
-	Build      string
-	Env        map[string]string
-	RemotePath string
-	RemoteAddr string
-	LocalPath  string
+type localData struct {
+	Developer    *schemas.Developer
+	Applications []*schemas.Application
 }
 
-func getApps() []*Application {
-	broome := &Application{
-		ID:    "80-maspeth-ave",
-		Name:  "Broome",
-		Start: "./broome",
-		Build: "make",
-		Env: map[string]string{
-			"ENV": "development",
-		},
-		RemotePath: "/home/bowery/broome",
-		LocalPath:  "/Users/david/Documents/gocode/src/github.com/Bowery/broome/",
+// Set up local db.
+func init() {
+	var err error
+	db, err = localdb.New(filepath.Join(sys.HomeVar, ".bowery_state"))
+	if err != nil {
+		log.Println("Unable to create local database.")
+		return
 	}
 
-	blog := &Application{
-		ID:         "110-fifth-ave",
-		Name:       "Blog",
-		Start:      "node app.js",
-		Build:      "npm install",
-		RemotePath: "/home/david/blog",
-		LocalPath:  "/Users/david/Documents/code/blog/",
-	}
+	// data = new(localData)
+	// if err := db.Load(data)
 
-	return []*Application{broome, blog}
+	// db.Load()
 }
 
 func main() {
-	fmt.Println("Starting Client")
-	r := render.New(render.Options{
-		IndentJSON:    true,
-		IsDevelopment: true, // TODO (thebyrd) remove in production
-	})
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		r.HTML(w, http.StatusOK, "home", map[string]interface{}{
-			"Title":  "Home Page!",
-			"Apps":   getApps(),
-			"Status": "All Systems Go!",
-		})
-	})
-
-	mux.HandleFunc("/applications/", func(w http.ResponseWriter, req *http.Request) {
-		id := req.URL.Path[len("/applications/"):]
-		var application *Application
-		for _, app := range getApps() {
-			if app.ID == id {
-				application = app
-				break
-			}
-		}
-		r.HTML(w, http.StatusOK, "application", map[string]interface{}{
-			"Application": application,
-			"Status":      "Syncing...",
-		})
-	})
+	mux.HandleFunc("/", indexHandler)
 
 	app := negroni.Classic()
 	app.UseHandler(mux)
 	app.Run(":3001")
+}
+
+func indexHandler(rw http.ResponseWriter, req *http.Request) {
+	r.HTML(rw, http.StatusOK, "home", map[string]interface{}{
+		"Title":  "Home Page!",
+		"Status": "All Systems Go!",
+	})
 }
