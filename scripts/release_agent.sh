77384941177b97777d300a422ce762476276021e
Larz Conwell <larzconwell@gmail.com>
2014-08-01 14:42:22 -0400

    Refactor agent release script

--- scripts/release_agent.sh
+++ scripts/release_agent.sh
@@ -1,91 +1,61 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
-
-# Get the parent directory of where this script is.
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )/../"
-CGO_ENABLED=0
-
-DIR="$DIR/bowery/agent"
-
-# Change into that dir because we expect that
-cd "$DIR"
-
-# Determine the version that we're building based on the contents
-# of delancey/VERSION.
-VERSION=$(cat ../VERSION)
-VERSIONDIR="${VERSION}"
-echo "Version: ${VERSION}"
-
-# Determine the arch/os combos we're building for
-XC_ARCH=${XC_ARCH:-"386 amd64 arm"}
-XC_OS=${XC_OS:-linux}
-
-echo "Arch: ${XC_ARCH}"
-echo "OS: ${XC_OS}"
 
 # Make sure that if we're killed, we kill all our subprocseses
 trap "kill 0" SIGINT SIGTERM EXIT
 
-# Make sure goxc is installed
+root="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../"
+version="$(cat "${root}/bowery/VERSION")"
+mkdir -p "${root}/bowery/agent/pkg/${version}/dist"
+cd "${root}/bowery/agent"
+echo "Version: ${version}"
+
+# Setup goxc for compiling.
 go get github.com/laher/goxc
+XC_ARCH=${XC_ARCH:-"386 amd64 arm"}
+XC_OS=${XC_OS:-linux}
+echo "Arch: ${XC_ARCH}"
+echo "OS: ${XC_OS}"
 
-# This function builds whatever directory we're in...
-goxc \
-    -tasks-="validate" \
-    -arch="$XC_ARCH" \
-    -os="$XC_OS" \
-    -d="${DIR}/pkg" \
-    -pv="${VERSION}" \
-    $XC_OPTS \
-    go-install \
-    xc
+goxc -arch="${XC_ARCH}" -os="${XC_OS}" -pv="${version}" -d=pkg xc
 
-# tar+gzip all the packages
-mkdir -p "./pkg/${VERSIONDIR}/dist"
-for PLATFORM in $(find "./pkg/${VERSIONDIR}" -mindepth 1 -maxdepth 1 -type d); do
-    PLATFORM_NAME=$(basename ${PLATFORM})
-    ARCHIVE_NAME="${VERSIONDIR}_${PLATFORM_NAME}"
-
-    if [ $PLATFORM_NAME = "dist" ]; then
-        continue
+# Compress the binaries.
+for platform in "pkg/${version}/"*; do
+    platform="$(basename "${platform}")"
+    if [[ "${platform}" == dist ]]; then
+      continue
     fi
+    archive="${version}_${platform}.tar.gz"
 
-    pushd ${PLATFORM}
-    tar -czf "${DIR}/pkg/${VERSIONDIR}/dist/${ARCHIVE_NAME}.tar.gz" ./*
-    popd
+    cd "pkg/${version}/${platform}"
+    tar -cvzf "../dist/${archive}" *
+    cd -
 done
 
-echo $VERSION > "./pkg/${VERSIONDIR}/dist/VERSION"
-cp -r "${DIR}/init/" "./pkg/${VERSIONDIR}/dist/"
-cp "${DIR}/install_agent.sh" "./pkg/${VERSIONDIR}/dist/"
+# Write release support files.
+echo "${version}" > "pkg/${version}/dist/VERSION"
+cp agent.conf install_agent.sh "pkg/${version}/dist"
+cd "pkg/${version}/dist"
+shasum -a256 * > "${version}_SHA256SUMS"
+cd -
 
-# Make the checksums
-pushd "./pkg/${VERSIONDIR}/dist"
-shasum -a256 * > "./${VERSIONDIR}_SHA256SUMS"
-popd
-
-for ARCHIVE in ./pkg/${VERSION}/dist/*; do
-    ARCHIVE_NAME=$(basename ${ARCHIVE})
-    echo Uploading: $ARCHIVE_NAME from $ARCHIVE
-    file=$ARCHIVE_NAME
-    bucket=bowery.sh
-    resource="/${bucket}/${file}"
-    contentType="application/octet-stream"
-    dateValue=`date -u +"%a, %d %h %Y %T +0000"`
-    stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
-    s3Key=AKIAI6ICZKWF5DYYTETA
-    s3Secret=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
-    signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
-    curl -k\
-        -T ${ARCHIVE} \
-        -H "Host: ${bucket}.s3.amazonaws.com" \
-        -H "Date: ${dateValue}" \
-        -H "Content-Type: ${contentType}" \
-        -H "Authorization: AWS ${s3Key}:${signature}" \
-        https://${bucket}.s3.amazonaws.com/${file}
-    echo
+for path in "pkg/${version}/dist/"*; do
+  file="$(basename "${path}")"
+  echo "Uploading: ${file} from ${path}"
+  bucket=bowery.sh
+  resource="/${bucket}/${file}"
+  contentType="application/octet-stream"
+  dateValue=`date -u +"%a, %d %h %Y %T +0000"`
+  stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+  s3Key=AKIAI6ICZKWF5DYYTETA
+  s3Secret=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
+  signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+  curl -k \
+    -T "${path}" \
+    -H "Host: ${bucket}.s3.amazonaws.com" \
+    -H "Date: ${dateValue}" \
+    -H "Content-Type: ${contentType}" \
+    -H "Authorization: AWS ${s3Key}:${signature}" \
+     https://${bucket}.s3.amazonaws.com/${file}
+  echo
 done
-
-exit 0
