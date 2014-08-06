e544c445bfde53db5442b89acdd88e318b83eda6
David Byrd <byrd@bowery.io>
2014-08-06 11:12:28 -0400

    gofmt

--- bowery/agent/sys_windows.go
+++ bowery/agent/sys_windows.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "errors"
+	"errors"
 )
 
 func GetPidTree(cpid int) (*Proc, error) {
-  return nil, errors.New("OS is not supported")
+	return nil, errors.New("OS is not supported")
 }
